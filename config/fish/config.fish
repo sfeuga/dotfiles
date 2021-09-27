0d108bffb5a92f34c8311a3fee5bb6bae0a74fb4
Stéphane FEUGA OSHIMA <sfeuga@member.fsf.org>
2021-09-27 12:24:30 +0900

    [ADD] asdf version manager

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,13 @@
+# RVM
 set -U fish_greetingrvm default
+
+# ASDF
+#   https://asdf-vm.com/guide/getting-started.html#_2-download-asdf
+if test -f ~/.asdf/asdf.fish
+    source ~/.asdf/asdf.fish
+
+    if not test -f ~/.config/fish/completions/asdf.fish
+        mkdir -p ~/.config/fish/completions && \
+        ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
+    end
+end
