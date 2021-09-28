850edf8e03290c7acfb09e3e3decf9d869b5cd1a
Stéphane FEUGA OSHIMA <stephane.oshima@wealth-park.com>
2021-09-28 11:52:13 +0900

    [UPDATE] cd function

--- config/fish/functions/cd.fish
+++ config/fish/functions/cd.fish
@@ -1,27 +1,27 @@
 function cd -d "Set venv/rvm source when cd"
 
-    if test "$argv" = "-"
-      set argv $old_pwd
-      set -gx old_pwd (pwd)
-      builtin cd $argv
-    else
-      set -gx old_pwd (pwd)
-      builtin cd $argv
-    end
+  if test "$argv" = "-"
+    set argv $old_pwd
+    set -gx old_pwd (pwd)
+    builtin cd $argv
+  else
+    set -gx old_pwd (pwd)
+    builtin cd $argv
+  end
 
-    ### Python & venv
-    if test -f ./venv/bin/activate
-        source "./venv/bin/activate.fish"
-    else
-        if type -q deactivate
-            deactivate 1>/dev/null 2>&1
-        end
+  ### Python & venv
+  if test -f ./venv/bin/activate
+    source "./venv/bin/activate.fish"
+  else
+    if type -q deactivate
+      deactivate 1>/dev/null 2>&1
     end
+  end
 
-    ### Ruby & RVM
-    if type -q rvm
-        if test -f ./.ruby-gemset
-            rvm gemset use (cat .ruby-gemset)
-        end
+  ### Ruby & RVM
+  if test -e ~/.rvm
+    if test -f ./.ruby-gemset
+      rvm gemset use (cat .ruby-gemset)
     end
+  end
 end
