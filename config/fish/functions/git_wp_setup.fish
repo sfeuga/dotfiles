36a02c280d74c6316810c8aabb779ab6e8b09ef7
Stéphane FEUGA OSHIMA <stephane.oshima@wealth-park.com>
2021-09-28 11:54:32 +0900

    [UPDATE] Tab in git_wp_setup function

--- config/fish/functions/git_wp_setup.fish
+++ config/fish/functions/git_wp_setup.fish
@@ -1,7 +1,7 @@
 ### Setup account to sign git commit for WealthPark
 function git_wp_setup
- 	if test -e .git
- 		command git config --local user.email stephane.oshima@wealth-park.com
-    	command git config --local user.signingKey 2AD5054BE235EBE6
- 	end
- end
+  if test -e .git
+    command git config --local user.email stephane.oshima@wealth-park.com
+    command git config --local user.signingKey 2AD5054BE235EBE6
+  end
+end
