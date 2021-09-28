f4c4b371afcf095eae28c84d8418da74d6b19e9e
Stéphane FEUGA OSHIMA <stephane.oshima@wealth-park.com>
2021-09-29 00:56:44 +0900

    [UPDATE] dev_folders function & [DELETE] git_wp_setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,16 +1,16 @@
 # RVM
 if test -f ~/.rvm
-    set -U fish_greetingrvm default
+  set -U fish_greetingrvm default
 end
 
 # ASDF
 #   https://asdf-vm.com/guide/getting-started.html#_2-download-asdf
 if test -f ~/.asdf/asdf.fish
-    source ~/.asdf/asdf.fish
+  source ~/.asdf/asdf.fish
 
-    if not test -f ~/.config/fish/completions/asdf.fish
-        mkdir -p ~/.config/fish/completions && \
-        ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
+  if not test -f ~/.config/fish/completions/asdf.fish
+    mkdir -p ~/.config/fish/completions && \
+      ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions/asdf.fish
     end
 end
 
--- config/fish/functions/git_wp_setup.fish
+++
@@ -1,5 +0,0 @@
-### Setup account to sign git commit for WealthPark
-function git_wp_setup
-  command git config --local user.email stephane.oshima@wealth-park.com
-  command git config --local user.signingKey 2AD5054BE235EBE6
-end
