5602ec2d01485a31eff3a900ebc5e511abd2799a
Stéphane FEUGA OSHIMA <sfeuga@member.fsf.org>
2022-02-06 18:01:27 +0900

    [UPDATE] sysup command to update global asdf apps

--- config/fish/conf.d/sysup.fish
+++ config/fish/conf.d/sysup.fish
@@ -16,8 +16,41 @@ else
   set flatpak ''
 end
 
-if test -d '/home/sfo/.asdf'
-  set asdf "; and set_color -b white; set_color black; echo ' ASDF: '; set_color normal; and asdf plugin-update --all"
+function asdf_update --description "An easy way to update your default app"
+  if test -f "$HOME/.tool-versions"
+    for app in (cat "$HOME/.tool-versions")
+      set app_name (echo "$app" | awk '{print $1}')
+      set app_version (echo "$app" | awk '{print $2}')
+
+      # Java is a bit different, so we need to change the name to the flavor we use
+      if test 'java' = "$app_name"
+        # I'm using openjdk
+        set app_latest_version (asdf latest java openjdk)
+      else
+        set app_latest_version (asdf latest "$app_name")
+      end
+
+      echo "Checking $app_name:"
+      if test "$app_version" = "$app_latest_version"
+        echo -e "\tNo Update"
+      else
+        echo -e "\tCurrent version:\t$app_version\n\tLatest version: \t$app_latest_version"
+
+        if test "$app_version" = "$app_latest_version"
+          echo "Do you want to install this new version? (y/N)"
+          read install
+          if test "$install" = 'y'; or "$install" = 'Y'
+            asdf install "$app_name" "$app_latest_version"
+            asdf global "$app_name" "$app_latest_version"
+          end
+        end
+      end
+    end
+  end
+end
+
+if test -d "$HOME/.asdf"
+  set asdf "; and set_color -b white; set_color black; echo ' ASDF: '; set_color normal; and asdf plugin-update --all; and asdf_update"
 else
   set asdf ''
 end
