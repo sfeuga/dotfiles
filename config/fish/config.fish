f4e4639f99558c7675f307fde69a93ad282b07bb
Stéphane FEUGA OSHIMA <sfeuga@member.fsf.org>
2022-02-06 19:36:14 +0900

    [UPDATE] config to set ls (& ll, la, lsa) & cat aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,20 @@
+# Set aliases in a Fish way
+function ls --description 'List contents of directory using exa'
+  command exa -F --icons $argv
+end
+function la --description 'List contents of directory using exa, including hidden files in directory using long format'
+  command exa -aF --icons $argv
+end
+function ll --description 'List contents using exa of directory using long format'
+  command exa -Fl --icons $argv
+end
+function lsa --description 'List contents using exa of directory and hidden files using long format'
+  command exa -aFl --icons $argv
+end
+function cat --description 'Cat clone with syntax highlighting and Git integration'
+  command bat $argv
+end
+
 # ASDF
 #   https://asdf-vm.com/guide/getting-started.html#_2-download-asdf
 if test -f ~/.asdf/asdf.fish
